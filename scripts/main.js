cdfba20884c8f19a2d4a93ae4cd5a4c5a84e184d
Lluis Casamajor <lluischess@gmail.com>
2021-04-01 17:36:52 +0200

    favicon + scroll up functionality

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,21 @@ const hamburgerMenu = document.querySelector('#nav');
 const navContent = document.querySelector('#nav-content');
 const closenavbutton = document.querySelector('#nav-content .close-btn');
 const navlinks = document.querySelectorAll('#nav-content nav ul li a');
+const scrollbutton = document.querySelector('.scroll-top');
+
+if (scrollbutton) {
+    window.addEventListener('scroll', ()=> {
+        if (pageYOffset > (window.innerHeight * 1.1)) {
+            scrollbutton.style.display = 'flex';
+             console.log(pageYOffset);
+        } else {
+            scrollbutton.style.display = 'none';
+        }
+    });
+    scrollbutton.addEventListener('click', ()=> {
+        window.scrollTo( 0, 0 );
+    })
+}
 
 hamburgerMenu.addEventListener('click', ()=> {
     navContent.classList.add('show');

325893a5c66c952df8c49081e94a9cab5b29b672
Lluis Casamajor <lluischess@gmail.com>
2021-04-01 17:10:14 +0200

    end nav menu functionalitys

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const hamburgerMenu = document.querySelector('#nav');
 const navContent = document.querySelector('#nav-content');
 const closenavbutton = document.querySelector('#nav-content .close-btn');
+const navlinks = document.querySelectorAll('#nav-content nav ul li a');
 
 hamburgerMenu.addEventListener('click', ()=> {
     navContent.classList.add('show');
@@ -10,4 +11,11 @@ hamburgerMenu.addEventListener('click', ()=> {
 closenavbutton.addEventListener('click', ()=>{
     navContent.classList.remove('show');
     document.body.style.overflow="initial";
+})
+
+navlinks.forEach( link => {
+    link.addEventListener('click', ()=>{
+        navContent.classList.remove('show');
+        document.body.style.overflow="initial";
+    })
 })

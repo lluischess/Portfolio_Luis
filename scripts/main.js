bef70403f286897e356eb5cbbe212df390dec775
Lluis Casamajor <lluischess@gmail.com>
2021-04-01 17:03:14 +0200

    javascript and css nav functional

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
-const hamburgerMenu = document.querySelector('#nav .nav-icon');
-const navConetent = document.querySelector('#nav-content');
+const hamburgerMenu = document.querySelector('#nav');
+const navContent = document.querySelector('#nav-content');
+const closenavbutton = document.querySelector('#nav-content .close-btn');
 
+hamburgerMenu.addEventListener('click', ()=> {
+    navContent.classList.add('show');
+    document.body.style.overflow="hidden";
+})
+
+closenavbutton.addEventListener('click', ()=>{
+    navContent.classList.remove('show');
+    document.body.style.overflow="initial";
+})
